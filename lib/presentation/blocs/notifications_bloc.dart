d67bca7efe8acbfb3850b109bea7f3b817b5026a
Franks Gonzalez <gonzalezfranks@hotmail.com>
2026-05-05 10:56:35 +0200

    add: Gestión de notificaciones push en Background y Foreground

--- lib/presentation/blocs/notifications_bloc.dart
+++ lib/presentation/blocs/notifications_bloc.dart
@@ -12,11 +12,11 @@ class NotificationsBloc extends Bloc<NotificationsEvent, NotificationsState> {
   final FirebaseMessaging firebaseMessaging = FirebaseMessaging.instance;
 
   NotificationsBloc() : super( const NotificationsState()) {
-    on<NotificationsEvent>((event, emit) {
-      // TODO: implement event handler
-    });
 
     on<NotificationStatusChanged>( _notificationStatusChanged );
+
+    _initialStatusCheck();
+    _onForegroundMessage();
   }
 
   static Future<void> initializeFCM() async {
@@ -33,6 +33,42 @@ class NotificationsBloc extends Bloc<NotificationsEvent, NotificationsState> {
       )
     );
 
+    //Enseñamos token para FCM por consola
+    _getFCMToken();
+
+  }
+
+  void _handleRemoteMessage(RemoteMessage message){
+    print("Tengo un mensaje en foreground");
+    print("Los datos el mensaje son:  ${message.data}");
+
+    if(message.notification == null) return;
+
+    print("El mensaje tambien contiene una notificacion ${message.notification}");
+  }
+
+  //Con esto gestiono las notificaciones en foreground y background
+  void _onForegroundMessage(){
+    FirebaseMessaging.onMessage.listen(_handleRemoteMessage);
+  }
+
+  void _initialStatusCheck () async {
+
+    //Obtenemos las settings actuales
+    final settings = await firebaseMessaging.getNotificationSettings();
+    //Y las indicamos mediante evento
+    add(NotificationStatusChanged(settings.authorizationStatus));
+
+  }
+
+  void _getFCMToken() async {
+    //Si no estoy autorizado no obtenemos el token
+    if( state.status != AuthorizationStatus.authorized ) return;
+
+    final token = await firebaseMessaging.getToken();
+
+    print("Token:");
+    print(token);
   }
 
   void requestPermissions() async {
